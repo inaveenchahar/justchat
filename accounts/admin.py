9870205da2ee8997b8d69cca877efafd292ff7e1
naveen <inaveenchahar@gmail.com>
2021-01-17 14:59:25 +0530

    feat(accounts): Profile model created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ProfileModel
 
 # Register your models here.
+
+
+@admin.register(ProfileModel)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'age', 'added_on', 'updated_on']
+    search_fields = ['user']
+    list_per_page = 20
+    list_filter = ['gender']
--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class ProfileModel(models.Model):
+    GENDER = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Others', 'Others')
+    )
+    AGE = [(i, i) for i in range(16, 101)]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_images/', null=True)
+    gender = models.CharField(choices=GENDER, null=True, max_length=8)
+    age = models.IntegerField(choices=AGE, null=True)
+    added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
 
