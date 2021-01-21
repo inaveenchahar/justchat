0776968780c6804cc6a9b8f35b0e47f4b690ebb5
naveen <inaveenchahar@gmail.com>
2021-01-21 20:09:18 +0530

    feat(main): pagination added,, if else added

--- main/views.py
+++ main/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from accounts.models import ProfileModel
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
 def home(request):
-    all_user = ProfileModel.objects.all().exclude(user__username=request.user.username)
-        # .exclude(username=request.user.username)
-    return render(request, 'home.html', {'all_user': all_user})
+    all_user = ProfileModel.objects.all().exclude(user=request.user).order_by('-user__last_login')
+    paginator = Paginator(all_user, 24)  # Show 24 users per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home.html', {'page_obj': page_obj})
 
 
 def profile_detail(request, username):
