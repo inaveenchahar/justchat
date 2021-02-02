10445fa97c1abd59dcb2eb3fbb0d1b5a7c530057
naveen <inaveenchahar@gmail.com>
2021-02-02 19:37:01 +0530

    fix(accounts): no profile pic bug fixed

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ class ProfileModel(models.Model):
     )
     AGE = [(i, i) for i in range(16, 101)]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_images/', null=True)
+    profile_pic = models.ImageField(upload_to='profile_images/', null=True, blank=True)
     gender = models.CharField(choices=GENDER, null=True, max_length=8)
     age = models.IntegerField(choices=AGE, null=True)
     bio = models.CharField(max_length=100, blank=True)
